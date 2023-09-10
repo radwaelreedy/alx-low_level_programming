4caf0c1d68d7e9fa23a70ce57ab29359e3895a20
radwaelreedy <radwa6503@gmail.com>
2023-09-10 10:13:49 -0700

    help

--- 5-printf.c
+++ 5-printf.c
@@ -5,13 +5,12 @@
  *
  * Description: A C program that prints with printf function.
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
 	printf(with proper grammar, but the outcome is a piece of art, \n);
 	return (0);
-
 }
 
