ff21f719fcd289cc14d44676c8f79ecd02eb6233
radwaelreedy <radwa6503@gmail.com>
2023-09-13 19:19:11 -0700

    help

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * times_table - prints the 9 times table
- * 
+ *
  * Example Table
  * 0, 0, 0, 0, ..
  * 0, 1, 2, 3, ..
@@ -20,9 +20,9 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(',');
-			
+
 			prod = num * mult;
-			
+
 			if (prod <= 9)
 				_putchar(' ');
 			else
