3ecf5c65258c2d68662dfad04c43f8298c6494b6
radwaelreedy <radwa6503@gmail.com>
2023-09-14 18:39:21 -0700

    hello world

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,7 +18,7 @@ void print_diagonal(int n)
 		for (postn = 1; postn <= n; postn++)
 		{
 			for (space = 1; space <= postn; space++)
-				_purchar(' ');
+				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
 		}
