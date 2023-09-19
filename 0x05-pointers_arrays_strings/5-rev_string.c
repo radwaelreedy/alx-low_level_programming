9856f681129c1bd9430faaebebea5a11cc65d2f0
radwaelreedy <radwa6503@gmail.com>
2023-09-18 19:37:24 -0700

    help

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ void rev_string(char *s)
 	int l, i;
 	char temp;
 
-	for (l = 0; s[l] != '\0'; l++)
+	for (l = 0; s[l] != '\0'; ++l)
 		;
 	for (i = 0; i < 1 / 2; i++)
 	{
@@ -21,5 +21,4 @@ void rev_string(char *s)
 		s[i] = s[l - 1 - i];
 		s[l - 1 - i] = temp;
 	}
-
 }
